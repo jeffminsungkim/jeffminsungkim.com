097da03ae7f5eba80da9944268bc8b13742c57b9
JeffMinsungKim <ziznzkak@gmail.com>
2018-03-26 00:34:57 +0900

    fix(test): format the code

--- test/unit/components/Helloworld.test.js
+++ test/unit/components/Helloworld.test.js
@@ -1,23 +1,23 @@
-import Vue from 'vue';
-import jsdom from 'jsdom';
-import HelloWorldComponent from '../../../components/HelloWorld.vue';
+import Vue from "vue";
+import jsdom from "jsdom";
+import HelloWorldComponent from "../../../components/HelloWorld.vue";
 
-const renderer = require('vue-server-renderer').createRenderer();
+const renderer = require("vue-server-renderer").createRenderer();
 
-describe('Test suite for HelloWorldComponent', () => {
-  it('should return Hello World! greeting message', () => {
+describe("Test suite for HelloWorldComponent", () => {
+  it("should return Hello World! greeting message", () => {
     const ClonedComponent = Vue.extend(HelloWorldComponent);
     const cc = new ClonedComponent({
       data() {
         return {
-          greeting: 'Hello World!',
+          greeting: "Hello World!"
         };
-      },
+      }
     }).$mount();
     renderer.renderToString(cc, (err, str) => {
       const dom = new jsdom.JSDOM(str);
-      const firstHeading = dom.window.document.querySelector('h1');
-      expect(firstHeading.textContent).toContain('Hello World!');
+      const firstHeading = dom.window.document.querySelector("h1");
+      expect(firstHeading.textContent).toContain("Hello World!");
     });
   });
 });
