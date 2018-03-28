b04cbcda6e9b058fe9efb490996288fc973cad68
JeffMinsungKim <ziznzkak@gmail.com>
2018-03-28 13:46:44 +0900

    add footer component

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <span class="copy-right">
+      <span role="img" aria-label="Copyright">©</span>
+      Minsung Kim
+    </span>
+  </div>
+</template>
+
+<style>
+.copy-right {
+  font-size: .9rem;
+}
+</style>
