957599decd427600a59e258dcad17678f651a0d2
JeffMinsungKim <ziznzkak@gmail.com>
2018-04-01 02:17:57 +0900

    remove style

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -13,15 +13,3 @@ export default {
   }
 };
 </script>
-
-<style scoped>
-
-.container {
-  min-height: 50vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: #33333d;
-}
-
-</style>
