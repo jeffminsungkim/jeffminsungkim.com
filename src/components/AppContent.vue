cf269b41fb5e69f0a0925ac698062884bcb0d2c2
JeffMinsungKim <ziznzkak@gmail.com>
2018-04-01 02:17:18 +0900

    test content

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -6,111 +6,147 @@
       </div>
     <div class="content">
       <h1 id="introduction">Introduction</h1>
-      <em>This website built within less than a day, by using <a href="https://github.com/nuxt/nuxt.js">Nuxt.js</a> and other amazing tools.</em>
-      <p>
-        I've finally completed 🎉 creating a personal website from scratch in early 2018.
+      <h3 class="sub-head">See <a href="https://github.com/jeffminsungkim/jeffminsungkim.com">source code</a> on Github.</h3>
+      <div class="jmk-blockquote">
         The purpose of creating this website is to share things that I'm interested in
         and also to describe how seriously I care about doing things on web development.
-      </p>
-      <h2>Planning Phase</h2>
-      <p>
-        By the time I write, usage of static websites is increasing in popularity.
-        In fact, it's not a big surprise as static site generators seem to have been gaining more popular among web developers these days too.
-      </p>
-      <p>
-        Before I get started building a website, based on the purpose of my website I thought it would be nice to get constructed with a static site.
-        I was wavering between two frameworks <a href="https://www.gatsbyjs.org/">Gatsby.js</a> and <a href="https://nuxtjs.org/">Nuxt.js</a>.
-      </p>
-      <p>
-        Gatsby.js is a static PWA (Progressive Web App) generator for React.
-        I've been seeing a lot of people talking about Gatsby on Twitter, and everyone seems to love it.
-      </p>
-      <div class="line-breaker">
-        <span>🤔 </span>
-        <em>Check out the following
-          <a href="https://medium.com/codingthesmartway-com-blog/gatsby-static-site-generator-for-react-introduction-b9fce7df6b24">Medium post</a>
-          if you're interested in reading about Gatsby.js in details.
-        </em>
+        Furthermore, I believe it's a wonderful opportunity for me to get my hands dirty to enhance my front-end skills.
       </div>
+      <img src="~/assets/image/jmk-pia.png" alt="Programming is Awesome!">
+      <h2>TL;DR</h2>
       <p>
-        Nuxt.js is a framework for creating Universal <a href="https://vuejs.org/">Vue.js</a> Applications.
-        Its main scope is <strong>UI rendering</strong> while abstracting away the client/server distribution.
-        Also, the Nuxt offers server rendering or static file rendering to boost the performance of your web application.
+        I've finally launched 🎉 a super simple fanciest personal website in early 2018.
+        This website built within less than a day, by using <a href="https://github.com/nuxt/nuxt.js">Nuxt.js</a> and other amazing tools.
+        Now I'm going to share the story of how I ended up with building this website.
       </p>
-      <div class="line-breaker">
-        <span>🤔 </span>
-        <em>Check out the following
-          <a href="https://medium.com/codingthesmartway-com-blog/introduction-to-server-rendered-vue-js-apps-with-nuxt-91dbfd80795a">Medium post</a>
-          if you're interested in reading about Nuxt.js in details.
-        </em>
-      </div>
-      <p>
-        As you may have noticed it seems like they both provide Server-Side Rendering feature!
-        So before moving on to the next step, let's briefly talk about what the server-side rendering is, and let's find out the real benefit.
-      </p>
-      <div class="line-breaker">
-        <h2>What is Server Side Rendering?</h2>
-        <p>
-          Server-side rendering (SSR) is the process of taking a client-side JavaScript framework website and
-          rendering it to static HTML and CSS on the server.
-        </p>
-        <img src="~/assets/image/jmk-static-html-css.png" alt="Static HTML and CSS">
-        <em>To find out more about benefits of
-          <a href="https://medium.com/walmartlabs/the-benefits-of-server-side-rendering-over-client-side-rendering-5d07ff2cefe8">
-          Server Side Rendering VS., Client Side Rendering</a>, check out the brilliant Medium post written by Alex Grigoryan.
-        </em>
-      </div>
-      <div class="line-breaker">
-        <h3>Why is this important?</h3>
-        <p>Most designers and developers tend to focus on building better aesthetic design or adding new nifty functionality on web pages.</p>
-        <p>People, however, want fast-loading websites, and server-side rendering is a tool to help you get your site rendered faster!</p>
-        <em>Be aware that page loading time is a critical factor in any website's user experience and moreover when it comes to search engine rankings.</em>
-      </div>
-
-      <div>
-      <h3>Pros and Cons</h3>
-      <p>
-        Compared to a traditional SPA (Single-Page Application), the advantage of SSR primarily lies in:
-        <ul>
-          <li>Better SEO, as the search engine crawlers will directly see the fully rendered page.</li>
-          <li>
-            Faster time-to-content, especially on slow internet or slow devices.
-            Server-rendered markup doesn't need to wait until all JavaScript has been downloaded and executed to be displayed.
-          </li>
-        </ul>
-      </p>
-      <p>
-        There are also some trade-offs to consider when using SSR:
-        <ul>
-          <li>
-            Development constraints.
-            Browser-specific code can only be used inside certain lifecycle hooks; some external libraries may need
-            special treatment to be able to run in a server-rendered app.
-          </li>
-          <li>
-            More involved build setup and deployment requirements.
-            Unlike a fully static SPA that can be deployed on any static file server,
-            a server-rendered app requires an environment where a Node.js server can run.
-          </li>
-          <li>
-            More server-side load.
-            Rendering a full app in Node.js is obviously going to be more CPU-intensive than just serving static files,
-            so if you expect high traffic, be prepared for corresponding server load and wisely employ caching strategies.
-          </li>
-        </ul>
-      </p>
-      </div>
-
-      <h2>Building Phase</h2>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi sint labore modi cumque illo unde ab dolor ut iusto! Atque?</p>
-      <img src="~/assets/image/jmk-huge-fan-of-js.png" alt="I'm a huge fan of JavaScript!">
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum eius expedita ut sapiente id nisi deserunt ex iusto doloribus saepe!</p>
     </div>
+    </section>
+    <section>
+      <div class="content">
+        <h1 id="how-i-built">How I Built 🛠</h1>
+        <h2 class="sub-head">Thought process while implementing my website</h2>
+        <p class="post-date"><small>March 29, 2018</small></p>
+
+        <h2>How I get started</h2>
+        <p>
+          At the time of writing, usage of static websites is increasing in popularity.
+          In fact, it's not a big surprise as static site generators seem to have been gaining more popular among web developers these days too.
+        </p>
+        <p>
+          For some reason, I had a chance to read one of the blog
+          <a href="https://www.netlify.com/blog/2017/05/25/top-ten-static-site-generators-of-2017/">posts</a>
+          on Netlify which was thankfully written by Eli Williamson.
+          After finish reading his article thought it would be nice to get constructed with a static site
+          before I dive into building my first website.
+        </p>
+
+        <h2>Static Site Generators</h2>
+        <p>
+          As I mentioned above, static site generators are one of the most popular options to create personal websites for developers.
+          They've been around for a while, but have recently started gaining some serious steam.
+        </p>
+        <p>
+          First thing first, I had to pick one of the
+          <a href="https://github.com/myles/awesome-static-generators">static site generators</a> to make the magic happen on my website.
+          <a href="https://jekyllrb.com/">Jekyll</a> holds it’s position at #1 as the most popular static site generator, but I wasn't really into it.
+          Meanwhile, two frameworks called
+          <a href="https://www.gatsbyjs.org/">Gatsby.js</a> and
+          <a href="https://nuxtjs.org/">Nuxt.js</a> draw my attention while reading the post that I mentioned a minute ago.
+        </p>
+        <p>
+          Gatsby.js is a static PWA (Progressive Web App) generator for React.
+          I've been seeing a lot of people talking about Gatsby on Twitter, and everyone seems to love it.
+        </p>
+        <div class="line-breaker">
+          <span>🤔 </span>
+          <em>Check out the following
+            <a href="https://medium.com/codingthesmartway-com-blog/gatsby-static-site-generator-for-react-introduction-b9fce7df6b24">Medium post</a>
+            if you're interested in reading about Gatsby.js in details.
+          </em>
+        </div>
+        <p>
+          Nuxt.js is a framework for creating Universal <a href="https://vuejs.org/">Vue.js</a> Applications.
+          Its main scope is <strong>UI rendering</strong> while abstracting away the client/server distribution.
+          Also, the Nuxt offers server rendering or static file rendering to boost the performance of your web application.
+        </p>
+        <div class="line-breaker">
+          <span>🤔 </span>
+          <em>Check out the following
+            <a href="https://medium.com/codingthesmartway-com-blog/introduction-to-server-rendered-vue-js-apps-with-nuxt-91dbfd80795a">Medium post</a>
+            if you're interested in reading about Nuxt.js in details.
+          </em>
+        </div>
+        <p>
+          As you may have noticed it seems like they both provide Server-Side Rendering feature!
+          So before moving on, let's briefly talk about what the server-side rendering is, and let's find out the real benefit.
+        </p>
+        <div class="line-breaker">
+          <h2>What is Server Side Rendering?</h2>
+          <p>
+            Server-side rendering (SSR) is the process of taking a client-side JavaScript framework website and
+            rendering it to static HTML and CSS on the server.
+          </p>
+          <img src="~/assets/image/jmk-static-html-css.png" alt="Static HTML and CSS">
+          <span>🤔 </span>
+          <em>
+            To find out more about benefits of
+            <a href="https://medium.com/walmartlabs/the-benefits-of-server-side-rendering-over-client-side-rendering-5d07ff2cefe8">
+            Server Side Rendering VS., Client Side Rendering</a>, check out a brilliant Medium post written by Alex Grigoryan.
+          </em>
+        </div>
+        <div class="line-breaker">
+          <h3>Why is this important?</h3>
+          <p>Most designers and developers tend to focus on building better aesthetic design or adding new nifty functionality on web pages.</p>
+          <p>People, however, want fast-loading websites, and server-side rendering is a tool to help you get your site rendered faster!</p>
+          <em>Be aware that page loading time is a critical factor in any website's user experience and moreover when it comes to search engine rankings.</em>
+        </div>
+
+        <div class="line-breaker">
+        <h3>Pros and Cons</h3>
+        <p>
+          Compared to a traditional SPA (Single-Page Application), the advantage of SSR primarily lies in:
+          <ul>
+            <li>Better SEO, as the search engine crawlers will directly see the fully rendered page.</li>
+            <li>
+              Faster time-to-content, especially on slow internet or slow devices.
+              Server-rendered markup doesn't need to wait until all JavaScript has been downloaded and executed to be displayed.
+            </li>
+          </ul>
+        </p>
+        <p>
+          There are also some trade-offs to consider when using SSR:
+          <ul>
+            <li>
+              Development constraints.
+              Browser-specific code can only be used inside certain lifecycle hooks; some external libraries may need
+              special treatment to be able to run in a server-rendered app.
+            </li>
+            <li>
+              More server-side load.
+              Rendering a full app in Node.js is obviously going to be more CPU-intensive than just serving static files,
+              so if you expect high traffic, be prepared for corresponding server load and wisely employ caching strategies.
+            </li>
+          </ul>
+        </p>
+        </div>
+
+        <h2>Decision</h2>
+        <p>
+          I was wavering between two frameworks Gatsby and Nuxt.
+        </p>
+        <span>🤔 </span>
+        <em>
+          To find out
+          <a href="https://medium.com/vue-mastery/10-reasons-to-use-nuxt-js-for-your-next-web-application-522397c9366b">
+          10 reasons to use Nuxt.js for your next web application</a>, check out a brilliant Medium post written by Derick Sozo.
+        </em>
+
+      </div>
     </section>
     <section>
     <div class="content">
       <h1 id="about">About Me 👋</h1>
-      <img src="~/assets/image/jmk-pia.png" alt="Programming is Awesome!">
+      <img src="~/assets/image/jmk-huge-fan-of-js.png" alt="I'm a huge fan of JavaScript!">
       <p>
         I feel confident about learning new technologies and
         especially interested in discovering hottest Front-end web application frameworks.
@@ -125,57 +161,25 @@
       </p>
     </div>
   </section>
-  <section>
-    <div class="content">
-      <h1 id="travel">Travel</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Iste beatae ipsa consequatur iure corrupti sapiente quam voluptates ducimus? Voluptates,
-        consectetur a? Optio ratione quibusdam fugit inventore ea dolore sequi qui tempora quia,
-        repellendus consequuntur vitae voluptatem iure! Eveniet illum dolores perspiciatis
-        maiores reiciendis accusamus reprehenderit doloribus in, nobis natus labore vitae ratione est quae
-        tenetur commodi delectus earum veniam fuga at. Dicta tenetur hic enim blanditiis non, cum laborum sunt?
-      </p>
-    </div>
-  </section>
   </div>
 </template>
 
 <style lang="scss" scoped>
 
-h1 {
-  margin-top: 0;
-  font-size: 2rem;
-}
-
-p {
-  font-size: .9rem;
-}
-
-em {
-  font-size: .85rem;
-}
-
-img {
-  width: 100%;
-  height: 100%;
-}
-
-ul {
-  padding: 0 20px;
-}
-
-li {
-  padding-bottom: 20px;
-}
-
-section {
-  @include content-section;
-}
-
 section .container-wrapper {
   padding-top: 20px;
   max-width: 2560px;
+}
+
+.sub-head {
+  opacity: .7;
+  font-size: 1.1rem;
+  color: #33333D;
+}
+
+.post-date {
+  opacity: .7;
+  text-align: right;
 }
 
 .line-breaker {
@@ -183,6 +187,15 @@ section .container-wrapper {
   padding-bottom: 30px;
 }
 
+.jmk-blockquote {
+  border-left: 3px solid rgba(0,0,0,.84);
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 20px;
+  margin: 15px;
+  margin-left: 5px;
+  font-style: italic;
+}
 
 .content {
   @include jmk-content;
@@ -200,8 +213,8 @@ section .container-wrapper {
     font-size: 1.1rem;
   }
   .image-wrapper {
-  padding: 0 100px;
-}
+    padding: 0 100px;
+  }
 }
 
 </style>
